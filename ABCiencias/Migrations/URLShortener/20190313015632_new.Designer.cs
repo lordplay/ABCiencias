d25f33365b88de0e920ce4088f2fcf6653a31cc8
Matheus Sansone Pinheiro <matheussanpinheiro@hotmail.com>
2019-03-12 22:58:50 -0300

    new migration

--- ABCiencias/Migrations/URLShortener/20190313015632_new.Designer.cs
+++ ABCiencias/Migrations/URLShortener/20190313015632_new.Designer.cs
@@ -3,14 +3,16 @@ using ABCiencias.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ABCiencias.Migrations.URLShortener
 {
     [DbContext(typeof(URLShortenerContext))]
-    partial class URLShortenerContextModelSnapshot : ModelSnapshot
+    [Migration("20190313015632_new")]
+    partial class @new
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ABCiencias/Migrations/URLShortener/20190313015632_new.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ABCiencias.Migrations.URLShortener
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Guid",
+                table: "Urls",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_Guid",
+                table: "Urls",
+                column: "Guid",
+                unique: true,
+                filter: "[Guid] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Urls_Guid",
+                table: "Urls");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Guid",
+                table: "Urls",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
